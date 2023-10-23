5e91b19c3cf2137ed7a192b8dab5e40f983e150e
Alfredo.Filigheddu <Alfredo Filigheddu@abirk.it>
2023-10-23 14:10:08 +0200

    feat: movie app footer
    Commited-by: @Alfrew

---
+++ src/app/core/components/ma-footer/ma-footer.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-ma-footer",
+  templateUrl: "./ma-footer.component.html",
+  styleUrls: ["./ma-footer.component.scss"],
+})
+export class MaFooterComponent implements OnInit {
+  currentRoute: string = "";
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
